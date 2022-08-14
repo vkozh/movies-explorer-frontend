160c6540dd1e760577b39da3eb6bc7b1cd84eff6
Lekozhe <v.a.kozh@ya.ru>
2022-08-14 18:58:01 +0300

    feat: movies list and search form

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,15 @@
+import React from "react"
+import FilterCheckbox from "./FilterCheckbox/FilterCheckbox"
+import "./SearchForm.css"
+
+export default function SearchForm() {
+  return (
+    <div className="section search-form">
+      <div className="search-form__input-bar input-bar">
+        <input type="text" placeholder="Фильм" className="input-bar__input" />
+        <button className="input-bar__button">Поиск</button>
+      </div>
+      <FilterCheckbox />
+    </div>
+  )
+}
