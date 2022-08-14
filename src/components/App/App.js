981d01513fc589ab92b03487e070dcc069c67713
Lekozhe <v.a.kozh@ya.ru>
2022-08-14 18:57:09 +0300

    feat: app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,99 @@
-import logo from '../../logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import Profile from "../Profile/Profile";
+import Register from "../Register/Register";
+import NotFound from "../NotFound/NotFound"
+import Login from "../Login/Login";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import { CurrentUserContext } from "../../utils/contexts/CurrentUserContext"
 
 function App() {
+
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState({ name: "Лера", email: "v.a.kozh@ya.ru" })
+  const navigate = useNavigate();
+
+  const handleSignin = ({ name, email }) => {
+    setIsloggedIn(true);
+    setCurrentUser({ name, email })
+  }
+
+  const handleSignup = ({ name, email, password }) => {
+    setIsloggedIn(true);
+    setCurrentUser({
+      name: name,
+      email: email
+    })
+  }
+
+  const handleLogout = () => {
+    setIsloggedIn(false);
+    setCurrentUser({});
+    navigate("/signin")
+  }
+
+  const handleEditProfile = ({ name, email }) => {
+    setCurrentUser({
+      name: name,
+      email: email
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Routes>
+          <Route
+            path="/movies"
+            element={
+              <Movies isLoggedIn={true} />
+            } />
+
+          <Route
+            path="/saved-movies"
+            element={
+              <SavedMovies
+                isLoggedIn={true}
+                isSavedMovies={true} />
+            } />
+
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                isLoggedIn={true}
+                logout={handleLogout}
+                editProfile={handleEditProfile}
+              />
+            } />
+
+          <Route
+            path="/signin"
+            element={
+              <Login signin={handleSignin} />
+            } />
+
+          <Route
+            path="/signup"
+            element={
+              <Register signup={handleSignup} />
+            } />
+
+          <Route
+            path="/"
+            exact
+            element={
+              <Main isLoggedIn={isLoggedIn} />
+            } />
+
+          <Route
+            path="*"
+            element={<NotFound />} />
+        </Routes>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
