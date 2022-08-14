2c5a4849458d01a53fa06556274464807ef6d70c
Lekozhe <v.a.kozh@ya.ru>
2022-08-14 18:53:08 +0300

    feat: menu

---
+++ src/components/Navigation/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from "react"
+import WithAuthorized from "./WithAuthorized/WithAuthorized";
+import MenuAuth from "./MenuAuth/MenuAuth";
+import MenuUnauth from "./MenuUnauth/MenuUnauth";
+
+export default function Menu({ isLoggedIn }) {
+  const MenuWithAuth = WithAuthorized(isLoggedIn, { MenuAuth, MenuUnauth })
+
+  return (
+    <MenuWithAuth />
+  )
+}
