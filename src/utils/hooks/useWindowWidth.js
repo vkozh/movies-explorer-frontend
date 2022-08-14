3fa0b8b4a7548de44c32c4da6c378f05caa6ade1
Lekozhe <v.a.kozh@ya.ru>
2022-08-14 18:54:37 +0300

    feat: custom hook and preloader

---
+++ src/utils/hooks/useWindowWidth.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+export default function useWindowWidth() {
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const handleScreenWidth = () => setScreenWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleScreenWidth)
+    return () => window.removeEventListener('resize', handleScreenWidth)
+  }, [])
+
+  return screenWidth;
+}
