f113d17c77525826aea8a7eea20c1504c9447e79
Lekozhe <v.a.kozh@ya.ru>
2022-08-16 10:23:36 +0300

    fix: add required to form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,11 @@ export default function SearchForm() {
   return (
     <div className="section search-form">
       <div className="search-form__input-bar input-bar">
-        <input type="text" placeholder="Фильм" className="input-bar__input" />
+        <input
+          type="text"
+          placeholder="Фильм"
+          className="input-bar__input"
+          required />
         <button className="input-bar__button">Поиск</button>
       </div>
       <FilterCheckbox />
